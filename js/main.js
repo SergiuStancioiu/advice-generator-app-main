1e9f8b8b3b5a572fe0b838ed5ce0214ef3182e98
Sergiu Stancioiu <s.sergiu96@gmail.com>
2023-10-19 23:20:57 +0300

    Fixed cache 'bug'

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-const apiUrl = "https://api.adviceslip.com/advice";
-const quoteRandomBtn = document.querySelector(".dice");
+document.addEventListener("DOMContentLoaded", () => {
+  const apiUrl = "https://api.adviceslip.com/advice";
+  const quoteRandomBtn = document.querySelector(".dice");
 
-quoteRandomBtn.addEventListener("click", () => {
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data.slip.advice);
-      console.log(data.slip.id);
-      randomAdvice(data);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  quoteRandomBtn.addEventListener("click", () => {
+    fetch(apiUrl, { cache: "no-store" })
+      .then((response) => response.json())
+      .then((data) => {
+        randomAdvice(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
 });
 
 function randomAdvice(data) {
   const adviceNumber = document.querySelector(".advice-number");
   const quote = document.querySelector(".quote");
 
-  adviceNumber.textContent = data.slip.id;
+  adviceNumber.textContent = "Advice #" + data.slip.id;
   quote.textContent = data.slip.advice;
 }
